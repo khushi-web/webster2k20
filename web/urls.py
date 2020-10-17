e026506c377559a034d84765fe2d191ce6f49f00
khushi <lestrangebellatrix11@gmail.com>
2020-10-16 21:03:16 -0700

    pay and email

--- web/urls.py
+++ web/urls.py
@@ -22,5 +22,11 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('accounts.urls'))
+    path('',include('accounts.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_complete"),
+    path('payment/', views.paypage,name="payement"),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
